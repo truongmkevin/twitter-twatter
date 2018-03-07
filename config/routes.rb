30b4fe1312cb33896541fb757a478bbc77670ec9
KT <truongmkevin@yahoo.com>
2018-03-07 16:26:46 +0000

    created tweet scaffold and added avatar uploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :tweets
+
+  get 'tweets' => 'tweet#index'
+  get 'tweets/new'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
