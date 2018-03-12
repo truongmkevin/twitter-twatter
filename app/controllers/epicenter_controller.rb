48b56aba7671e97f3ea9bc9ededcc20ecc569284
KT <truongmkevin@yahoo.com>
2018-03-12 13:41:22 +0000

    catching up on commits, created tag model, and tagtweet model, and migrated

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,13 +1,29 @@
 class EpicenterController < ApplicationController
   def feed
+
+    @following_tweets = []
+
+    Tweet.all.each do |tweet|
+      if current_user.following.include?(tweet.user_id) || current_user.id == tweet.user_id
+        @following_tweets.push(tweet)
+      end
+    end
+
+    # Personal better equivalent of above
+    # array_of_users = current_user.following
+    # array_of_users << current_user.id
+    # @following_tweets = Tweet.where(user_id: array_of_users)
+
   end
 
   def show_user
+    @user = User.find(params[:id])
   end
 
   def now_following
   end
 
   def unfollow
+    @name = params[:name]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'epicenter/feed'
+  root 'epicenter#feed'
 
-  get 'epicenter/show_user'
+  get 'show_user' => 'epicenter#show_user'
 
-  get 'epicenter/now_following'
+  get 'now_following' => 'epicenter#now_following'
 
-  get 'epicenter/unfollow'
+  get 'unfollow' => 'epicenter#unfollow'
 
   resources :tweets
-
-  root 'tweets#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
